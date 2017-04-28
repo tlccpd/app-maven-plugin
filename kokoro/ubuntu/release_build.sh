1322519966d28ad95e44b8a651f8448c3db87cca
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-04-28 15:38:09 -0400

    copy pom to target with new name

--- kokoro/ubuntu/release_build.sh
+++ kokoro/ubuntu/release_build.sh
@@ -9,3 +9,5 @@ cd github/app-maven-plugin
 sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 mvn -Prelease -B -U verify
+# copy pom with the name extected in the Maven repository
+cp pom.xml target/`mvn help:evaluate -Dexpression=project.artifactId | grep -v "^\["`-`mvn help:evaluate -Dexpression=project.version | grep -v "^\["`.pom
