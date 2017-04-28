b2ecb8dcc9864364e6c7fa573c362b844e8206ae
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-04-28 16:20:07 -0400

    avoid mvn download progress output

--- kokoro/ubuntu/release_build.sh
+++ kokoro/ubuntu/release_build.sh
@@ -10,4 +10,4 @@ sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 mvn -Prelease -B -U verify
 # copy pom with the name extected in the Maven repository
-cp pom.xml target/`mvn help:evaluate -Dexpression=project.artifactId | grep -v "^\["`-`mvn help:evaluate -Dexpression=project.version | grep -v "^\["`.pom
+cp pom.xml target/`mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null| grep -v "^\["`-`mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\["`.pom
