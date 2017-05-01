23a61ee34f8655dee029f2c4049c6e3fa9ab2146
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-01 15:47:25 -0400

    add sign job

--- kokoro/ubuntu/release_build.sh
+++ kokoro/ubuntu/release_build.sh
@@ -11,5 +11,3 @@ sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 mvn -Prelease -B -U verify
 # copy pom with the name extected in the Maven repository
 cp pom.xml target/`mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null| grep -v "^\["`-`mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\["`.pom
-
-gsutil ls gs://app-maven-plugin-release
---
+++ kokoro/ubuntu_sign/release_sign.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+mkdir -p signed && chmod 777 signed
+
+# find the latest directory under prod/app-maven-plugin/gcp_ubuntu/release/
+LAST_BUILD=$(ls prod/app-maven-plugin/gcp_ubuntu/release/ | sort -rV | head -1)
+# find regular files under the directory, assumption is that only the jars and
+# the pom are there
+FILES=$(find `pwd`prod/app-maven-plugin/gcp_ubuntu/release/${LAST_BUILD}/* -type f)
+for f in $FILES
+do
+  echo "Processing $f file..."
+  filename=$(basename "$f")
+  if /escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t linux_gpg_sign \
+    $f
+    "signed/$filename"
+  then echo "Signed $filename"
+  else
+    echo "Could not sign $filename"
+  fi
+done
